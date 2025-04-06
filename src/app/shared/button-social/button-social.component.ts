290b761c5a4ada6775cfaa7eb54f5420a2eebad8
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-06 12:16:42 +0330

    feat: add social button component

---
+++ src/app/shared/button-social/button-social.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-social',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-social.component.html',
+  styleUrl: './button-social.component.scss'
+})
+export class ButtonSocialComponent {
+
+}
