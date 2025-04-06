358671eee5d5141299e4157f6a686a307dd24f1e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-06 12:56:26 +0330

    feat: add validation to input

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonPrimaryComponent } from '../shared/button-primary/button-primary.component';
 import { ButtonSocialComponent } from '../shared/button-social/button-social.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ButtonPrimaryComponent, ButtonSocialComponent],
+  imports: [ButtonPrimaryComponent, ButtonSocialComponent, FormsModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {}
+export class CardComponent {
+  constructor() {}
+
+  onSubmit() {}
+}
